bf30f63ca809aa5a2817d98bb09b28d5eb94766d
Liam Sherratt <RC0026314@student.riversidecollege.ac.uk>
2017-09-13 10:20:41 +0100

    display name along with saying hello

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,33 +29,46 @@
         private void InitializeComponent()
         {
             this.Hello = new System.Windows.Forms.Button();
+            this.Txtbxname = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Hello
             // 
-            this.Hello.Location = new System.Drawing.Point(134, 12);
+            this.Hello.Location = new System.Drawing.Point(202, 12);
             this.Hello.Name = "Hello";
             this.Hello.Size = new System.Drawing.Size(87, 27);
             this.Hello.TabIndex = 0;
-            this.Hello.Text = "Hello";
+            this.Hello.Text = " Hello";
             this.Hello.UseVisualStyleBackColor = true;
             this.Hello.Click += new System.EventHandler(this.Hello_Click);
+            // 
+            // Txtbxname
+            // 
+            this.Txtbxname.Location = new System.Drawing.Point(202, 123);
+            this.Txtbxname.Name = "Txtbxname";
+            this.Txtbxname.Size = new System.Drawing.Size(100, 20);
+            this.Txtbxname.TabIndex = 1;
+            this.Txtbxname.Text = "d";
+            this.Txtbxname.TextChanged += new System.EventHandler(this.Txtbxname_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(369, 300);
+            this.ClientSize = new System.Drawing.Size(570, 547);
+            this.Controls.Add(this.Txtbxname);
             this.Controls.Add(this.Hello);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button Hello;
+        private System.Windows.Forms.TextBox Txtbxname;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,7 +19,12 @@ namespace WindowsFormsApp1
 
         private void Hello_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World");
+            
+        }
+
+        private void Txtbxname_TextChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello  " + Txtbxname.Text);
         }
     }
 }
